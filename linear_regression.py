bbb0baa0efbb39961bd4ffb0549967887540a624
Your Name <you@example.com>
2024-10-24 01:01:48 +0300

    init

--- linear_regression.py
+++ linear_regression.py
@@ -14,4 +14,19 @@ class MyLineReg():
         return f"MyLineReg class: n_iter={self.n_iter}, learning_rate={self.learning_rate}"
 
     def fit(self, X: pd.DataFrame, y: pd.Series, verbose:int=False):
-        123
+        X = pd.concat([pd.DataFrame([[1]] * len(X)), X], axis=1)
+        X.columns = range(X.shape[1])
+        self.weights = [1] * (X.shape[1] + 1)
+
+        for i in range(self.n_iter):
+            predictions = [sum(list(Xes) * self.weights) for Xes in X.iterrows()]
+
+            loss = 1/len(X) * sum((predictions - y)**2)
+
+            if verbose:
+                print(f"Iteration {i+1}/{self.n_iter}, Loss: {loss}")
+
+regression = MyLineReg(1000,0.2)
+X = pd.DataFrame([[1,2,3,4],[1,2,3,4],[1,2,3,4]])
+y = pd.Series([1,2,3])
+regression.fit(X,y)
